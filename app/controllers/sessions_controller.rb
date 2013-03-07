97b1b5fdb08cb9cc72c5e8fc59471b51adb7ed09
Michael Crismali <michael.crismali@gmail.com>
2013-03-07 09:00:44 -0600

    added actions to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
 
+  def create
+    @user = User.find_by_email(params[:email])
+
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_url, :notice => "Sign-in successful."
+    else
+      redirect_to new_session_url, :notice => "Sign-in unsuccessful."
+    end
+  end
+
+  def new
+    #login form/page
+  end
+
+  def destroy
+    reset_session
+  end
 
 end
