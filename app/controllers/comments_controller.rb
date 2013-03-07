5fda8b1eaa1da79aacd33e554688c71582ed04b2
Michael Crismali <michael.crismali@gmail.com>
2013-03-07 09:02:48 -0600

    copied over comments controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
 
+  def create
+    @comment = Comment.new(params[:comment])
+
+    if @comment.save
+
+      if @comment.tyml.sender_id == @comment.user.id
+        redirect_to sent_url
+      else
+        redirect_to dashboard_url
+      end
+
+    else
+      redirect_to dashboard_url
+    end
+
+  end
 
 end
