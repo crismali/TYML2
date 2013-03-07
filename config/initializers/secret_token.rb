5853c16c851aeb6da89c8bef7ddb7e023fc17488
Michael Crismali <michael.crismali@gmail.com>
2013-03-06 20:55:14 -0600

    first commit.
    essentially I'll copy and paste a ton of stuff from the other TYML and
    recreate a better version of it. This is an attempt to remove devise
    and clean things up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TYML2::Application.config.secret_token = 'e55a09a49751f069abe9dec59e484ae30360e9a3e21cec86ec35079388c4a8cb396fc5a0300d5bac06d09b31cbcafa16401a45828c04fa71b1cd346c8485f4e5'
