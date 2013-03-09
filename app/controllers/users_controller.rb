d8d5da62879e59a65d9a66b9bce9b4108abfeb12
Michael Crismali <michael.crismali@gmail.com>
2013-03-08 20:01:02 -0600

    added authorize user to users controller and experimented with autocomplete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
 
+  before_filter :authorize_user
+
+  def authorize_user
+    if session[:user_id].nil?
+      redirect_to new_session_url
+    end
+  end
+
+
   def dashboard
     current_user
+    @autocomplete_items = @current_user.contacts.map { |x| x.contact.email}
+    # @autocomplete_items = @current_user.contacts.map do |x|
+    #   z = Array.new
+    #   z << x.contact.email if x.contact.name.nil?
+    #   z << x.contact.name unless x.contact.name.nil?
+    #   z << x.contact_id
+    #   z
+    # end
   end
 
   def sent
