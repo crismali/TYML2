e751eb1e5cb9a6f008ff614f6daf54729fe5d219
Michael Crismali <michael.crismali@gmail.com>
2013-03-08 20:00:18 -0600

    added create action to tymls controller

--- app/controllers/tymls_controller.rb
+++ app/controllers/tymls_controller.rb
@@ -1,4 +1,15 @@
 class TymlsController < ApplicationController
 
+  def create
+    @tyml = Tyml.new(params[:tyml])
+
+    @tyml.save
+
+    respond_to do |format|
+      format.js
+    end
+
+  end
+
 
 end
