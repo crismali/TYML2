963e594b426fa03f608966b8b38438c3d99b7a13
Michael Crismali <michael.crismali@gmail.com>
2013-03-10 18:25:44 -0500

    added check to make sure users are confirmed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
 
-    if @user.present? && @user.authenticate(params[:password])
+    if @user.present? && @user.confirmed && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_url, :notice => "Sign-in successful."
     else
