66e030c8612dc9546e5c0fe18a3a553f03c40261
Michael Crismali <michael.crismali@gmail.com>
2013-03-10 21:03:36 -0500

    tymls controller figures out ids of receivers, creates them if non existant, and adds them to their contacts

--- app/controllers/tymls_controller.rb
+++ app/controllers/tymls_controller.rb
@@ -3,7 +3,37 @@ class TymlsController < ApplicationController
   def create
     @tyml = Tyml.new(params[:tyml])
 
-    @tyml.save
+    receiver_ids, new_receiver_emails = @tyml.get_receiver_ids
+
+    receiver_ids.each do |a_receiver_id|
+      new_tyml = Tyml.new(params[:tyml])
+      new_tyml.receiver_id = a_receiver_id
+      new_tyml.save
+    end
+
+    new_receiver_emails.each do |receiver_email|
+      user = User.find_by_email(receiver_email)
+      new_tyml = Tyml.new(params[:tyml])
+      c = Contact.new
+      if user
+        c.user_id = @tyml.sender_id
+        c.contact_id = user.id
+        c.save
+        new_tyml.receiver_id = user.id
+        new_tyml.save
+      else
+        user = User.new
+        user.email = receiver_email
+        if user.save
+          new_tyml.receiver_id = user.id
+          new_tyml.save
+          c.user_id = @tyml.sender_id
+          c.contact_id = user.id
+          c.save
+        end
+      end
+    end
+
 
     respond_to do |format|
       format.js
