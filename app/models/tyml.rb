06555dc2661dab2b3428a90a61cb948caa542744
Michael Crismali <michael.crismali@gmail.com>
2013-03-10 21:05:18 -0500

    added get receiver ids method to tymls

--- app/models/tyml.rb
+++ app/models/tyml.rb
@@ -5,4 +5,20 @@ class Tyml < ActiveRecord::Base
   belongs_to :sender, :class_name => 'User', :foreign_key => 'sender_id'
   belongs_to :receiver, :class_name => 'User', :foreign_key => 'receiver_id'
   has_many :comments
+
+  def get_receiver_ids
+    receiver_ids = Array.new
+    receiver_names_emails = receiver_id.split(',')
+    sender.contacts.each do |c|
+      if receiver_names_emails.include?(c.contact.name)
+        receiver_ids << c.contact_id
+        receiver_names_emails.delete_at(receiver_names_emails.index(c.contact.name))
+      elsif receiver_names_emails.include?(c.contact.email)
+        receiver_ids << c.contact_id
+        receiver_names_emails.delete_at(receiver_names_emails.index(c.contact.email))
+      end
+    end
+    return receiver_ids.uniq!, receiver_names_emails
+  end
+
 end
