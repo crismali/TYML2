ac5a5de43b708c3927c0e90053c3a8f0b560aa44
Michael Crismali <michael.crismali@gmail.com>
2013-03-10 19:11:36 -0500

    rake db:migrated (had to make confirmed mass assignable for seeds fil)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :confirmed
   attr_accessible :bio, :email, :location, :name, :profile_pic
 
   validates_presence_of :email
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130307031050) do
+ActiveRecord::Schema.define(:version => 20130310225859) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130307031050) do
     t.boolean  "viewed",      :default => false
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
+    t.boolean  "archived",    :default => false
   end
 
   create_table "users", :force => true do |t|
