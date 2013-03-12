3813d9841f93f4c40957a8cb52b544ec4e02c41a
Michael Crismali <michael.crismali@gmail.com>
2013-03-12 10:42:01 -0500

    copied over mailer stuff from TYML

--- app/mailers/tyml_mailer.rb
+++ app/mailers/tyml_mailer.rb
@@ -1,11 +1,20 @@
 class TymlMailer < ActionMailer::Base
   default from: "\"TYML\" <tyml@tyml.it>"
 
-  def notification(tyml)
+  def new_tyml_notification(tyml)
     @tyml = tyml
 
     mail to: @tyml.receiver_email, subject: "New TYML from #{@tyml.sender.name}"
   end
-end
 
+  def comment_notification(comment)
+    @comment = comment
+    @comment_receiver = User.find_by_email(comment.tyml.receiver_email)
+
+    if comment.user == comment.tyml.sender
+      mail to: @comment.tyml.receiver_email, subject: "TYML: New comment from #{@comment.tyml.sender.name}"
+    else
+      mail to: @comment.tyml.sender.email, subject: "TYML: New comment from #{@comment_receiver.name}"
+    end
+  end
 end
