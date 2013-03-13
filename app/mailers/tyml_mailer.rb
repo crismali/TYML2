6281e68fd4a759a7e3fe81f07d6142526b6a7c2e
Michael Crismali <michael.crismali@gmail.com>
2013-03-12 19:08:42 -0500

    updated mailers for new db (no more receiver email method)

--- app/mailers/tyml_mailer.rb
+++ app/mailers/tyml_mailer.rb
@@ -4,7 +4,7 @@ class TymlMailer < ActionMailer::Base
   def new_tyml_notification(tyml)
     @tyml = tyml
 
-    mail to: @tyml.receiver_email, subject: "New TYML from #{@tyml.sender.name}"
+    mail to: @tyml.receiver.email, subject: "New TYML from #{@tyml.sender.name}"
   end
 
   def comment_notification(comment)
@@ -12,7 +12,7 @@ class TymlMailer < ActionMailer::Base
     @comment_receiver = User.find_by_email(comment.tyml.receiver_email)
 
     if comment.user == comment.tyml.sender
-      mail to: @comment.tyml.receiver_email, subject: "TYML: New comment from #{@comment.tyml.sender.name}"
+      mail to: @comment.tyml.receiver.email, subject: "TYML: New comment from #{@comment.tyml.sender.name}"
     else
       mail to: @comment.tyml.sender.email, subject: "TYML: New comment from #{@comment_receiver.name}"
     end
