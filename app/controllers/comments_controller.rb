ee613d14a18e7638cda4a7d31d2c75634c9290d7
Michael Crismali <michael.crismali@gmail.com>
2013-03-12 19:08:20 -0500

    commented out mailer, made comments#create only respond to js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,9 @@ class CommentsController < ApplicationController
 
     if @comment.save
       @tyml = @comment.tyml
-      TymlMailer.comment_notification(@comment).deliver
+     # TymlMailer.comment_notification(@comment).deliver
 
       respond_to do |format|
-        format.html { redirect_to dashboard_url }
         format.js
       end
     end
