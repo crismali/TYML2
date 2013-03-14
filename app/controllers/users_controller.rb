70f383702e82c2cbf7f481a4adc22f0a41814cdc
Michael Crismali <michael.crismali@gmail.com>
2013-03-14 13:42:43 -0500

    made autocomplete items a callback and made received/sent tymls only select those that aren't 'deleted' or archived

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :authorize_user
+  before_filter :authorize_user, :get_autocomplete_items
 
   def authorize_user
     if current_user.nil?
@@ -8,12 +8,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def get_autocomplete_items
+    @autocomplete_items = @current_user.autocomplete_contacts
+  end
+
   def dashboard
-    @autocomplete_items = current_user.autocomplete_contacts
+    @received_tymls = @current_user.received_tymls.where(archived: false, receiver_deleted: false).order('created_at desc')
   end
 
   def sent
-    @autocomplete_items = current_user.autocomplete_contacts
+    @sent_tymls = @current_user.sent_tymls.where(sender_deleted: false).order('created_at desc')
   end
 
   def edit
