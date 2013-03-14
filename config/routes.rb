6bc02c86f33be1681a758b743ec12c5686d49f71
Michael Crismali <michael.crismali@gmail.com>
2013-03-14 12:19:08 -0500

    replaced matches with more specific requests (get, delete, puts, post)

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,20 @@ TYML2::Application.routes.draw do
   root :to => 'Users#dashboard'
 
   resources :sessions, :only => [ :create, :new, :destroy]
-  match '/signin' => 'sessions#new', :as => 'signin'
-  match '/sign-out' => 'sessions#destroy', :as => 'signout'
+  get '/signin' => 'sessions#new', :as => 'signin'
+  delete '/sign-out' => 'sessions#destroy', :as => 'signout'
 
   resources :create_accounts, :only => [ :new, :create, :update ]
-  match '/signup' => 'create_accounts#new', :as => 'signup'
-  match '/signup/:reset_password_token' => 'create_accounts#update', :as => 'confirm'
-  match '/welcome' => 'create_accounts#welcome', :as => 'welcome'
+  get '/signup' => 'create_accounts#new', :as => 'signup'
+  put '/signup/:reset_password_token' => 'create_accounts#update', :as => 'confirm'
+  get '/welcome' => 'create_accounts#welcome', :as => 'welcome'
 
   resources :users, :only => [ :update, :edit ]
   get '/dashboard', :controller => 'Users', :action => 'dashboard', :as => 'dashboard'
   get '/dashboard/sent', :controller => 'Users', :action => 'sent', :as => 'sent'
   get '/dashboard/contacts', :controller => 'Users', :action => 'contacts', :as => 'contacts'
-  match '/dashboard/settings' => 'users#edit', :as => 'settings'
-  match '/dashboard/contacts' => 'contacts#index'
+  get '/dashboard/settings' => 'users#edit', :as => 'settings'
+  get '/dashboard/contacts' => 'contacts#index'
 
   resources :comments, :only => [ :create, :update, :destroy ]
 
