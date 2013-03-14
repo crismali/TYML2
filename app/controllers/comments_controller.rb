001b8fd6b46bf2c4a6a8d632c671dfe8d268d31e
Michael Crismali <michael.crismali@gmail.com>
2013-03-13 20:05:07 -0500

    moved mail notification to after save callback in comments model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,6 @@ class CommentsController < ApplicationController
     @comment = Comment.new(params[:comment])
 
     if @comment.save
-      @tyml = @comment.tyml
-     # TymlMailer.comment_notification(@comment).deliver
-
       respond_to do |format|
         format.js
       end
