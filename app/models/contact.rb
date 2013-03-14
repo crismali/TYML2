213cccca7803ca87b8a1a7e339d4b3d124491b49
Michael Crismali <michael.crismali@gmail.com>
2013-03-14 12:23:40 -0500

    removed custom validation and replaced it with a smarter validates uniquness of method call

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,14 +3,7 @@ class Contact < ActiveRecord::Base
 
   validates_presence_of :contact_id, :user_id
 
-  validate :unique_pair_of_users
-
-  def unique_pair_of_users
-    if Contact.where(user_id: self.user_id, contact_id: self.contact_id).count > 1
-      errors.add(:contact_id, "That person is already a contact.")
-      logger.info(errors.inspect)
-    end
-  end
+  validates_uniqueness_of :user_id, :scope => [:contact_id]
 
   belongs_to :user
   belongs_to :contact, class_name: 'User', foreign_key: 'contact_id', primary_key: 'id'
