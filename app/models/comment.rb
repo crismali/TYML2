64a9c605c4fc5937eeec656b073519ba655b9de8
Michael Crismali <michael.crismali@gmail.com>
2013-03-13 20:05:29 -0500

    added send_notification for after saves

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,11 @@ class Comment < ActiveRecord::Base
   validates_presence_of :comment_text, :tyml_id, :user_id
   belongs_to :tyml
   belongs_to :user
+
+  after_save :send_notification
+
+  def send_notification
+    TymlMailer.comment_notification(self).deliver
+  end
+
 end
