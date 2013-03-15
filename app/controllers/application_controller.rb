55f026cf014a17f6b80f5a0a9d793d93044989e4
Michael Crismali <michael.crismali@gmail.com>
2013-03-15 02:29:11 -0500

    current user method now explicitly returns @current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user = User.find_by_remember_token(cookies[:remember_token])
     @current_user ||= User.find_by_id(session[:user_id])
+    return @current_user
   end
 
 end
