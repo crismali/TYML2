0a54f97987607abfc1084e5e3659750e73bd4848
Michael Crismali <michael.crismali@gmail.com>
2013-03-15 01:35:14 -0500

    added remember me logic through cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,11 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
 
     if @user.present? && @user.confirmed && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+      if params[:remember_me] == '1'
+        cookies.permanent[:remember_token] = @user.remember_token
+      else
+        session[:user_id] = @user.id
+      end
       redirect_to root_url, :notice => "Sign-in successful."
     else
       redirect_to new_session_url, :notice => "Sign-in unsuccessful."
@@ -16,6 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    cookies.delete(:remember_token)
     reset_session
     redirect_to signin_url
   end
