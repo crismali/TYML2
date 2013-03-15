add181f7fdf105bb463fd924c909c9dcf1f27249
Michael Crismali <michael.crismali@gmail.com>
2013-03-15 01:36:57 -0500

    made current user method check for remember me cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    @current_user = User.find_by_id(session[:user_id])
+    @current_user = User.find_by_remember_token(cookies[:remember_token])
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
 end
