677a101546948b2c61ff880acbdd1f8ce99e6ad7
Michael Crismali <michael.crismali@gmail.com>
2013-03-16 23:57:27 -0500

    logging in saves ip address and time and generates a remember me token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,16 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-
     if @user.present? && @user.confirmed && @user.authenticate(params[:password])
+      @user.last_sign_in_ip = @user.current_sign_in_ip
+      @user.current_sign_in_ip = request.remote_ip
+      @user.last_sign_in_at = @user.current_sign_in_at
+      @user.current_sign_in_at = Time.now
+      @user.sign_in_count = @user.sign_in_count + 1
+      @user.save
       if params[:remember_me] == '1'
+        @user.remember_token = SecureRandom.urlsafe_base64
+        @user.save
         cookies.permanent[:remember_token] = @user.remember_token
       else
         session[:user_id] = @user.id
