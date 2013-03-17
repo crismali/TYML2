deaaca83ad76ddf1e0fd6ab9cde62c0ffe2470ac
Michael Crismali <michael.crismali@gmail.com>
2013-03-16 23:56:47 -0500

    current user method works for remember me and session logins and checks ip address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user = User.find_by_remember_token(cookies[:remember_token])
-    @current_user ||= User.find_by_id(session[:user_id])
+    @current_user = User.find_by_id(session[:user_id]) unless session[:user_id].nil?
+    if @current_user.present? && request.remote_ip != @current_user.current_sign_in_ip
+      @current_user = nil
+    end
     return @current_user
   end
 
