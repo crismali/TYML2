5dec271f85d7b298949d48b3d9b4a0f173b9e8cd
Michael Crismali <michael.crismali@gmail.com>
2013-03-19 12:49:42 -0500

    uncommented comments notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,7 @@ class Comment < ActiveRecord::Base
   after_save :send_notification
 
   def send_notification
-    #TymlMailer.comment_notification(self).deliver
+    TymlMailer.comment_notification(self).deliver
   end
 
 end
