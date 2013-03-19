6d55d00632c152854637cc4cc1b272e26bca2584
Michael Crismali <michael.crismali@gmail.com>
2013-03-18 21:51:20 -0500

    added account confirmation email to user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def account_confirmation_email(user)
+    @user = user
+    mail to: @user.email, subject: "Confirm your account with TYML"
+  end
+
+
 end
