4cff2f109f37b3fbc44a409394094922e96c8796
Michael Crismali <michael.crismali@gmail.com>
2013-03-19 12:55:35 -0500

    copied over mailer/sendgrid stuff from first tyml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,18 @@ TYML2::Application.configure do
 
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings =
+  {
+    :enable_starttls_auto => true,
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => 'tyml.app@gmail.com',
+    :password           => '70C#cKTp7YK^'
+  }
 end
