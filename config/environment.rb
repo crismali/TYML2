043ba2cf7eadebaf0463aa514796212ab31df619
Michael Crismali <michael.crismali@gmail.com>
2013-03-19 12:51:10 -0500

    copied over mailer/sendgrid stuff from first tyml

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TYML2::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "npostolovski",
+  :password => "F3Rw$mjL*uAu",
+  :domain => "tyml2.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
