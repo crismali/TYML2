c3f8224c306dad481a34bfffcb147a309bddf36d
Michael Crismali <michael.crismali@gmail.com>
2013-03-19 10:08:13 -0500

    added reset password email to user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,5 +6,10 @@ class UserMailer < ActionMailer::Base
     mail to: @user.email, subject: "Confirm your account with TYML"
   end
 
+  def reset_password_email(user)
+    @user = user
+    mail to: @user.email, subject: "Reset your TYML password"
+  end
+
 
 end
