d0f5a79b4d8799123d35669c2f225273d9d382bb
Natasha Postolovski <npostolovski@gmail.com>
2013-03-22 16:10:07 -0500

    Testing whether avatars persist.

--- app/uploaders/profile_pic_uploader.rb
+++ app/uploaders/profile_pic_uploader.rb
@@ -16,8 +16,7 @@ class ProfilePicUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  #
-  
+
   def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
