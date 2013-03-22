7f2ad763752069d337c7d55ddc7e05b7a0fd1185
Michael Crismali <michael.crismali@gmail.com>
2013-03-22 01:19:49 -0500

    won't receive email notification on tymls that are deleted

--- app/mailers/tyml_mailer.rb
+++ app/mailers/tyml_mailer.rb
@@ -10,9 +10,9 @@ class TymlMailer < ActionMailer::Base
   def comment_notification(comment)
     @comment = comment
 
-    if comment.user == comment.tyml.sender && @comment.tyml.receiver.receive_comment_notifications
+    if comment.user == comment.tyml.sender && @comment.tyml.receiver.receive_comment_notifications && !@comment.tyml.receiver_deleted
       mail to: @comment.tyml.receiver.email, subject: "TYML: New comment from #{@comment.tyml.sender.name}"
-    elsif @comment.tyml.sender.receive_comment_notifications
+    elsif @comment.tyml.sender.receive_comment_notifications && !@comment.tyml.sender_deleted
       mail to: @comment.tyml.sender.email, subject: "TYML: New comment from #{@comment.tyml.receiver.name}"
     end
   end
